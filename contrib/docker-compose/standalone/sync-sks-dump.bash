4da8a683a6d7ce1081455461fb6c0aa50ebdc3d0
Andrew Gallagher <andrew.gallagher@siren.io>
2021-05-11 12:14:59 +0100

    /bin/bash is not included in rsync-ssh

--- contrib/docker-compose/standalone/sync-sks-dump.bash
+++ contrib/docker-compose/standalone/sync-sks-dump.bash
@@ -3,4 +3,4 @@
 set -eu
 
 docker-compose up -d import-keys
-docker-compose exec import-keys /bin/bash -c "cd /import; rsync -avr rsync://rsync.cyberbits.eu/sks/dump ."
+docker-compose exec import-keys "rsync -avr rsync://rsync.cyberbits.eu/sks/dump /import/dump"
