8bcb908ccc2d94cfd65694730132cd10393f464d
Andrew Gallagher <andrew.gallagher@siren.io>
2021-06-28 14:34:02 +0100

    Migrate templating from sed to envsubst

--- contrib/docker-compose/standalone/mkconfig.bash
+++ contrib/docker-compose/standalone/mkconfig.bash
@@ -4,9 +4,8 @@ HERE=$(cd $(dirname $0); pwd)
 set -eux
 
 [ -e "$HERE/.env" ]
-. "$HERE/.env"
 
-sed 's/POSTGRES_USER/'"$POSTGRES_USER"'/;s/POSTGRES_PASSWORD/'"$POSTGRES_PASSWORD"'/;s/RELEASE/'"$RELEASE"'/;s/FQDN/'"$FQDN"'/;s/FINGERPRINT/'"$FINGERPRINT"'/;' \
-	"$HERE/hockeypuck/etc/hockeypuck.conf.tmpl" > "$HERE/hockeypuck/etc/hockeypuck.conf"
-sed 's/FQDN/'"$FQDN"'/;' \
-	"$HERE/nginx/conf.d/hockeypuck.conf.tmpl" > "$HERE/nginx/conf.d/hockeypuck.conf"
+env - $("$HERE/.env") envsubst \
+	< "$HERE/hockeypuck/etc/hockeypuck.conf.tmpl" > "$HERE/hockeypuck/etc/hockeypuck.conf"
+env - $("$HERE/.env") envsubst \
+	< "$HERE/nginx/conf.d/hockeypuck.conf.tmpl" > "$HERE/nginx/conf.d/hockeypuck.conf"
