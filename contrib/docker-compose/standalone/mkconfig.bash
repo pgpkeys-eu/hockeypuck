0e8778cf3ff221b463065a6431292a15cc57285d
Andrew Gallagher <andrew.gallagher@siren.io>
2021-06-30 11:47:16 +0100

    Fix various errata

--- contrib/docker-compose/standalone/mkconfig.bash
+++ contrib/docker-compose/standalone/mkconfig.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 HERE=$(cd $(dirname $0); pwd)
-set -eux
+set -eu
 
 [ -e "$HERE/.env" ]
 
-env - $("$HERE/.env") envsubst \
+env - $(< "$HERE/.env") envsubst '$FQDN:$FINGERPRINT:$RELEASE:$POSTGRES_USER:$POSTGRES_PASSWORD' \
 	< "$HERE/hockeypuck/etc/hockeypuck.conf.tmpl" > "$HERE/hockeypuck/etc/hockeypuck.conf"
-env - $("$HERE/.env") envsubst \
+env - $(< "$HERE/.env") envsubst '$FQDN' \
 	< "$HERE/nginx/conf.d/hockeypuck.conf.tmpl" > "$HERE/nginx/conf.d/hockeypuck.conf"
