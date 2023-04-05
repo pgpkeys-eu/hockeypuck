046e8bf5a381f08afd065a1b9987cfd38bcfb01f
Andrew Gallagher <andrew.gallagher@siren.io>
2023-04-05 08:55:47 +0100

    find-keys can now read patterns from stdin

--- contrib/docker-compose/standalone/find-keys.bash
+++ contrib/docker-compose/standalone/find-keys.bash
@@ -4,21 +4,6 @@
 
 set -euo pipefail
 
-if [[ ! ${1:-} ]]; then
-    cat <<EOF
-Usage: $0 [options] SEARCH_STRING
-
-Where options are:
-
--r  SEARCH_STRING is a regex (searches against first userid only)
--t  SEARCH_STRING is a formatted tsquery
-
-Otherwise SEARCH_STRING is a search-engine style webquery.
-
-EOF
-    exit 1
-fi
-
 # Uncomment and edit one of the below for your postgres installation
 # for docker-compose/standalone default configuration
 SQLCMD="docker exec -i standalone_postgres_1 psql hkp -U hkp"
@@ -27,10 +12,55 @@ SQLCMD="docker exec -i standalone_postgres_1 psql hkp -U hkp"
 # for non-docker postgres, e.g.
 #SQLCMD="psql hkp -U hkp"
 
-if [[ $1 == -r ]]; then
-    $SQLCMD -t <<<"select reverse(rfingerprint), keywords from keys where doc->'userIDs'->0->>'keywords' ~ '$2';"
-elif [[ $1 == -t ]]; then
-    $SQLCMD -t <<<"select reverse(rfingerprint), keywords from keys, to_tsquery($2) query where query @@ keywords;"
-else
+usage() {
+    cat <<EOF
+Usage: $0 [options] SEARCH
+
+If SEARCH is "-", then search parameters of the appropriate type are read from STDIN, one per line.
+
+Command line options are:
+
+-r  each search parameter is a regex (searches against first userid only)
+-t  each search parameter is a SQL tsquery
+
+Otherwise each search parameter is a search-engine style webquery.
+
+EOF
+    exit 1
+}
+
+s_userid_regex() {
+    $SQLCMD -t <<<"select reverse(rfingerprint), keywords from keys where doc->'userIDs'->0->>'keywords' ~ '$1';"
+}
+
+s_keywords_tsquery() {
+    $SQLCMD -t <<<"select reverse(rfingerprint), keywords from keys, to_tsquery($1) query where query @@ keywords;"
+}
+
+s_keywords_websearch() {
     $SQLCMD -t <<<"select reverse(rfingerprint), keywords from keys, websearch_to_tsquery('$1') query where query @@ keywords;"
+}
+
+[[ ${1:-} ]] || usage
+
+if [[ $1 == -r ]]; then
+    shift
+    [[ ${1:-} ]] || usage
+    COMMAND=s_userid_regex
+elif [[ $1 == -t ]]; then
+    shift
+    [[ ${1:-} ]] || usage
+    COMMAND=s_keywords_tsquery
+else
+    COMMAND=s_keywords_websearch
+fi
+
+if [[ $1 == "-" ]]; then
+    while read -r pattern ; do
+        [[ $pattern && "${pattern:0:1}" != "#" ]] || continue
+        echo "# $pattern"
+        $COMMAND "$pattern"
+    done
+else
+    $COMMAND "$1"
 fi
