3f6fac3f17cb28b77c7352c5eec3ef14998c9326
Andrew Gallagher <andrewg@andrewg.com>
2023-04-23 23:39:29 +0100

    Haproxy mksite/mkconfig simplification (#28)
    * Migrate haproxy config file housekeeping from mkconfig.bash to entrypoint
    
    * Move docker-compose specific envar defs into docker-compose.yml file

--- contrib/docker-compose/standalone/mkconfig.bash
+++ contrib/docker-compose/standalone/mkconfig.bash
@@ -13,7 +13,7 @@ set -eua
 . "$HERE/.env"
 
 # Check for migrations
-if ! grep -q MIGRATION_HAPROXY_LOGFORMAT_DONE "$HERE/.env"; then
+if ! grep -q MIGRATION_3_DONE "$HERE/.env"; then
 	cat <<EOF
 
 -----------------------------------------------------------------------
@@ -30,24 +30,3 @@ fi
 [ ! -f "$HERE/hockeypuck/etc/hockeypuck.conf" ] &&
 	envsubst '$FQDN:$FINGERPRINT:$RELEASE:$POSTGRES_USER:$POSTGRES_PASSWORD' \
 	< "$HERE/hockeypuck/etc/hockeypuck.conf.tmpl" > "$HERE/hockeypuck/etc/hockeypuck.conf"
-
-# Populate the LOCAL portions of the HAProxy configuration
-for template in "$HERE"/haproxy/etc/haproxy.d/*LOCAL*.cfg.tmpl; do
-	config="$HERE/haproxy/etc/haproxy.d/$(basename "$template" .tmpl)"
-	[ ! -f "$config" ] &&
-		cp "$template" "$config"
-done
-
-[ ! -d "$HERE/haproxy/etc/lists" ] &&
-	mkdir "$HERE/haproxy/etc/lists"
-
-# Make sure that black/whitelists exist, even if empty
-for file in blacklist whitelist prometheus_whitelist; do
-	[ ! -f "$HERE/haproxy/etc/lists/$file.list" ] &&
-		touch "$HERE/haproxy/etc/lists/$file.list"
-done
-
-# And populate the aliases map
-for alias in ${ALIAS_FQDNS:-} ${CLUSTER_FQDNS:-}; do
-	echo "$alias $FQDN"
-done > "$HERE"/haproxy/etc/lists/aliases.map
